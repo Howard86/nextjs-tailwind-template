9859bad8c9963b4b525e16ad91f1dbafd59b51b2
Howard Tai <howard@howardism.dev>
2022-02-06 13:22:57 +0800

    refactor: set up daisyui to replace custom tw class

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
 export default class Document extends NextDocument {
   render(): JSX.Element {
     return (
-      <Html>
+      <Html data-theme='light'>
         <Head />
         <body>
           <Main />
