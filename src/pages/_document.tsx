fabe3ae66baa1db6ba158d78c91812b71be463a5
Howard Tai <howard@howardism.dev>
2022-02-06 12:52:36 +0800

    refactor: set up page with tailwind css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,4 @@
-import { ColorModeScript } from '@chakra-ui/react';
 import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
-
-import theme from '@/theme';
 
 export default class Document extends NextDocument {
   render(): JSX.Element {
@@ -9,7 +6,6 @@ export default class Document extends NextDocument {
       <Html>
         <Head />
         <body>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,96 +1,113 @@
-import React from 'react';
-
-import {
-  Box,
-  Container,
-  Heading,
-  Link,
-  SimpleGrid,
-  Skeleton,
-  Text,
-} from '@chakra-ui/react';
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 
-import Image from '@/components/Image';
-import RouteLink from '@/components/RouteLink';
+import vercelLogo from '@/public/vercel.svg';
 import { useGetNameQuery } from '@/services/local';
 
-const Home = (): JSX.Element => {
-  const { data, isSuccess, fulfilledTimeStamp } = useGetNameQuery();
+const Home = () => {
+  const { data, fulfilledTimeStamp } = useGetNameQuery();
 
   return (
-    <Container>
-      <Box>
-        Go to <RouteLink href="/new-page">New Page</RouteLink>
-        <Skeleton isLoaded={isSuccess}>
-          Local API /hello responds {data?.name} called{' '}
-          {data?.timestamp || 0 - (fulfilledTimeStamp || 0)}ms ago
-        </Skeleton>
-      </Box>
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Box as="main">
-        <Heading as="h1">
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <Link href="https://nextjs.org" isExternal>
+          <a
+            className="text-blue-600 hover:underline"
+            href="https://nextjs.org"
+          >
             Next.js!
+          </a>
+        </h1>
+
+        <p className="mt-3 text-2xl">
+          Get started by editing{' '}
+          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
+            pages/index.tsx
+          </code>
+        </p>
+
+        <p className="text-sm">
+          Go to{' '}
+          <Link href="/new-page">
+            <a className="text-blue-600 hover:underline">New Page</a>
           </Link>
-        </Heading>
+        </p>
 
-        <Text>
-          Get started by editing <Text as="code">pages/index.js</Text>
-        </Text>
+        {data && fulfilledTimeStamp && (
+          <p>
+            Local API /hello responds {data.name} called{' '}
+            {fulfilledTimeStamp - data.timestamp}ms ago
+          </p>
+        )}
 
-        <SimpleGrid>
-          <Link href="https://nextjs.org/docs" isExternal>
-            <Heading as="h3">Documentation &rarr;</Heading>
-            <Text>
+        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+          <a
+            href="https://nextjs.org/docs"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <p className="mt-4 text-xl">
               Find in-depth information about Next.js features and API.
-            </Text>
-          </Link>
+            </p>
+          </a>
 
-          <Link href="https://nextjs.org/learn" isExternal>
-            <Heading as="h3">Learn &rarr;</Heading>
-            <Text>
+          <a
+            href="https://nextjs.org/learn"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
+            <p className="mt-4 text-xl">
               Learn about Next.js in an interactive course with quizzes!
-            </Text>
-          </Link>
+            </p>
+          </a>
 
-          <Link
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            isExternal
+          <a
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <Heading as="h3">Examples &rarr;</Heading>
-            <Text>
+            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
+            <p className="mt-4 text-xl">
               Discover and deploy boilerplate example Next.js projects.
-            </Text>
-          </Link>
+            </p>
+          </a>
 
-          <Link
+          <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            isExternal
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <Heading as="h3">Deploy &rarr;</Heading>
-            <Text>
+            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
+            <p className="mt-4 text-xl">
               Instantly deploy your Next.js site to a public URL with Vercel.
-            </Text>
-          </Link>
-        </SimpleGrid>
-      </Box>
+            </p>
+          </a>
+        </div>
+      </main>
 
-      <Box as="footer">
-        <Link
+      <footer className="flex h-24 w-full items-center justify-center border-t">
+        <a
+          className="flex items-center justify-center"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          isExternal
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={64} height={64} />
-        </Link>
-      </Box>
-    </Container>
+          <Image
+            src={vercelLogo}
+            alt="Vercel Logo"
+            className="ml-2 h-4"
+            height={16}
+            width={64}
+          />
+        </a>
+      </footer>
+    </div>
   );
 };
 
